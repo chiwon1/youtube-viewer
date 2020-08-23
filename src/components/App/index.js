5f8b454f23b4c4f8c3438e9d13c2517e4d7a09e7
Ken Huh <ken@vanillacoding.co>
2020-08-23 14:27:34 +0900

    Update searchinput and styled components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
+import SearchInput from "../SearchInput";
 import logo from "../../assets/logo.svg";
 import "./styles.css";
 
+const AppWrapper = styled.div`
+  text-align: center;
+
+  .app-logo {
+    height: 100px;
+  }
+
+  .app-header {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .input-container {
+    width: 300px;
+  }
+`;
+
+const Heading = styled.h1`
+  margin: 10px 0;
+  font-size: 2rem;
+  font-weight: 500;
+`;
+
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <AppWrapper>
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <Heading>Youtube Viewer</Heading>
+        <div className="input-container">
+          <SearchInput
+            placeholder="Youtube 검색"
+            onChange={(searchKeyword) => {
+              console.log(searchKeyword);
+            }}
+          />
+        </div>
       </header>
-    </div>
+    </AppWrapper>
   );
 }
--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,5 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+// import { debounce } from "lodash";
 
-export default function SearchInput() {
-  return <input type="text" placeholder="Hello," />;
+const Input = styled.input`
+  padding: 10px 15px;
+  border: 1px solid #ededed;
+  height: 24px;
+  min-width: 200px;
+  width: calc(100% - 32px);
+  border-radius: 2px;
+  font-size: 16px;
+  letter-spacing: -0.5px;
+  transition: 0.3s all ease;
+
+  &:hover,
+  &:focus,
+  &:active {
+    border: 1px solid #414141;
+  }
+`;
+
+export default function SearchInput({ onChange, placeholder, value }) {
+  return (
+    <Input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={(ev) => onChange(ev.target.value)}
+    />
+  );
 }
